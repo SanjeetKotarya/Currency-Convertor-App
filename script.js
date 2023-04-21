af43050fb5d37a9b5374e6e425f62caa26245e58
Sanjeet Kotarya <sanjeetkotarya@gmail.com>
2023-04-21 17:46:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const dropList = document.querySelectorAll("form select"),
+    fromCurrency = document.querySelector(".from select"),
+    fromInput = document.querySelector(".from input"),
+    toInput = document.querySelector(".to input"),
+    toCurrency = document.querySelector(".to select"),
+    getButton = document.querySelector("form button");
+    getunit = document.querySelector("form .unit");
+
+for (let i = 0; i < dropList.length; i++) {
+    for (let currency_code in country_list) {
+        let selected = i == 0 ? currency_code == "USD" ? "selected" : "" : currency_code == "NPR" ? "selected" : "";
+        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+        dropList[i].insertAdjacentHTML("beforeend", optionTag);
+    }
+
+}
+
+window.addEventListener("load", () => {
+    getExchangeRate();
+});
+
+getButton.addEventListener("click", e => {
+    e.preventDefault();
+    getExchangeRate();
+});
+
+function getExchangeRate() {
+    let fromInputVal = fromInput.value;
+    if (fromInputVal == "" || fromInputVal == "0") {
+        fromInput.value = "1";
+        fromInputVal = 1;
+    }
+    toInput.value = "Getting exchange rate...";
+    let url = `https://v6.exchangerate-api.com/v6/14abb0b17e8749d3d5b12c0a/latest/${fromCurrency.value}`;
+    fetch(url).then(response => response.json()).then(result => {
+       let exchangeRate = result.conversion_rates[toCurrency.value];
+        let totalExRate = (fromInputVal * exchangeRate).toFixed(2);
+        toInput.value = `${totalExRate}`;
+        getunit.innerHTML = "1 " +`${fromCurrency.value}`+" = "+`${exchangeRate}`+`${toCurrency.value}`;
+    });
+}
